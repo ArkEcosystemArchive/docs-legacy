16943caefa8968a563e545ecb109ab3230ee5d07
jeremiG <gendronjeremi@gmail.com>
2018-09-25 08:09:08 -0400

    ARK API => Ark API

--- docs/.vuepress/sidebars/api.js
+++ docs/.vuepress/sidebars/api.js
@@ -2,7 +2,7 @@ module.exports = [
   ['/', 'Back to Table of Contents'],
   ["/api/", "API Home"],
   ['/api/public/', 'Public API'],
-  ['/api/sdk/', 'ARK SDK'],
+  ['/api/sdk/', 'Ark SDK'],
   ['/api/json-rpc/', 'JSON RPC'],
   ['/api/p2p/', 'P2P'],
   '/api/webhooks/',
